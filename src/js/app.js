99f58c51c687aaf8898ab6f96784f23b103889ee
Grégoire Sage <gregoire.sage@gmail.com>
2016-02-21 15:30:38 +0100

    Update clay and simplify js

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,12 @@
-var appinfo = require('../../appinfo.json');
-
 var liveconfig = require('liveconfig');
 
 var Clay = require('clay');
 var clayConfig = require('config.json');
-// patch the configuration
-clayConfig.push({ "type": "text", "id" : "UUID", "defaultValue": appinfo.uuid });
-clayConfig.push({ "type": "text", "id" : "WATCHTOKEN", "defaultValue": Pebble.getWatchToken() });
-
 var customClay = require('custom-clay');
 var clay = new Clay(clayConfig, customClay, {autoHandleEvents: false});
 
 Pebble.addEventListener('showConfiguration', function(e) {
-  liveconfig.connect(appinfo.uuid, function(id, value) {
+  liveconfig.connect(Pebble.getAccountToken(), function(id, value) {
     var config = {};
     config[id] = value;
     console.log(JSON.stringify(config));
--- src/js/custom-clay.js
+++ src/js/custom-clay.js
@@ -5,21 +5,13 @@ module.exports = function(minified) {
   var HTML = minified.HTML;
 
   Clay.on(Clay.EVENTS.AFTER_BUILD, function() {
-    var uuid = Clay.getItemById('UUID').get();
-    var watchtoken = Clay.getItemById('WATCHTOKEN').get();
-    var connection = new WebSocket("wss://liveconfig.fletchto99.com/forward/" + uuid + "/" + watchtoken);
+    var connection = new WebSocket("wss://liveconfig.fletchto99.com/forward/" + Clay.meta.accountToken + "/" + Clay.meta.watchToken);
     connection.onopen = function() {
-        connection.send(JSON.stringify({id: 'background', value: 65535}));
-
-        var onchange = function() {
-          connection.send(JSON.stringify({id: this.id, value: this.get()}));
-        };
-      
-        Clay.getItemByAppKey('enableBackground').on('change', onchange);
-        Clay.getItemByAppKey('background').on('change', onchange);
+      Clay.getAllItems().map( function(item) {
+        item.on('change', function() {
+          connection.send(JSON.stringify({id: this.appKey, value: this.get()}));
+        });
+      });
     };
-    // hack to hide the texts
-    Clay.getItemById('UUID').set('');
-    Clay.getItemById('WATCHTOKEN').set('');
   });
 };
